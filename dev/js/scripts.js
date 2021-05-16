9a2d1246d9ef6d7e7c1c6eade99c6af161650c24
sdb329 <sdb329@drexel.edu>
2021-05-16 15:47:02 -0400

    final nav changes

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,18 @@
 
+
+import $ from "jquery";
+
+let $nav = $('nav[data-nav]');
+let isVisible = false;
+
+$(".nav-btns").on("click", function(){
+    // console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+})
